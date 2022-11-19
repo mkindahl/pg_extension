610fa9f41d7a274c04ea340bb2c6667ebbb9e8bf
Mats Kindahl <mats@timescale.com>
2022-11-19 12:03:38 +0100

    Fix to locate postgres binary
    The `postgres` binary could not be found using the hard-coded paths so
    instead we rely on `pg_config` to give us a binary directory where we
    can find the binary.
    
    Also fixing the formatting of the CMake files.

--- cmake/FindPostgreSQL.cmake
+++ cmake/FindPostgreSQL.cmake
@@ -13,7 +13,7 @@
 # the License.
 #
 # .rst: FindPostgreSQL
-# --------------
+# --------------------
 #
 # Find the PostgreSQL installation.
 #
@@ -21,11 +21,20 @@
 #
 # ::
 #
-# PostgreSQL_INCLUDE_DIRS - include directories PostgreSQL_SERVER_INCLUDE_DIRS -
-# include directories for server programming PostgreSQL_LIBRARY_DIRS  - link
-# directories for PostgreSQL libraries PostgreSQL_EXTENSION_DIR  - the directory
-# for extensions PostgreSQL_SHARED_LINK_OPTIONS  - options for shared libraries
+# PostgreSQL_LIBRARIES - the PostgreSQL libraries needed for linking
+#
+# PostgreSQL_INCLUDE_DIRS - include directories
+#
+# PostgreSQL_SERVER_INCLUDE_DIRS - include directories for server programming
+#
+# PostgreSQL_LIBRARY_DIRS  - link directories for PostgreSQL libraries
+#
+# PostgreSQL_EXTENSION_DIR  - the directory for extensions
+#
+# PostgreSQL_SHARED_LINK_OPTIONS  - options for shared libraries
+#
 # PostgreSQL_LINK_OPTIONS  - options for static libraries and executables
+#
 # PostgreSQL_VERSION_STRING - the version of PostgreSQL found (since CMake
 # 2.8.8)
 #
@@ -46,101 +55,107 @@ find_program(
 
 if(NOT PG_CONFIG)
   message(FATAL_ERROR "Could not find pg_config")
+else()
+  set(PostgreSQL_FOUND TRUE)
 endif()
 
 message(STATUS "Found pg_config as ${PG_CONFIG}")
 
-find_program(
-    PG_BINARY postgres
-    PATHS ${PostgreSQL_ROOT_DIRECTORIES}
-    PATH_SUFFIXES bin)
+if(PostgreSQL_FOUND)
+  macro(PG_CONFIG VAR OPT)
+    execute_process(
+      COMMAND ${PG_CONFIG} ${OPT}
+      OUTPUT_VARIABLE ${VAR}
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+  endmacro()
 
-if(NOT PG_BINARY)
-  message(FATAL_ERROR "Could not find postgres binary")
-endif()
+  pg_config(_pg_bindir --bindir)
+  pg_config(_pg_includedir --includedir)
+  pg_config(_pg_pkgincludedir --pkgincludedir)
+  pg_config(_pg_sharedir --sharedir)
+  pg_config(_pg_includedir_server --includedir-server)
+  pg_config(_pg_libs --libs)
+  pg_config(_pg_ldflags --ldflags)
+  pg_config(_pg_ldflags_sl --ldflags_sl)
+  pg_config(_pg_ldflags_ex --ldflags_ex)
+  pg_config(_pg_pkglibdir --pkglibdir)
+  pg_config(_pg_libdir --libdir)
+  pg_config(_pg_version --version)
 
-message(STATUS "Found postgres binary at ${PG_BINARY}")
+  separate_arguments(_pg_ldflags)
+  separate_arguments(_pg_ldflags_sl)
+  separate_arguments(_pg_ldflags_ex)
 
-macro(PG_CONFIG VAR OPT)
-  execute_process(
-    COMMAND ${PG_CONFIG} ${OPT}
-    OUTPUT_VARIABLE ${VAR}
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-endmacro()
+  set(_server_lib_dirs ${_pg_libdir} ${_pg_pkglibdir})
+  set(_server_inc_dirs ${_pg_pkgincludedir} ${_pg_includedir_server})
+  string(REPLACE ";" " " _shared_link_options
+                 "${_pg_ldflags};${_pg_ldflags_sl}")
+  set(_link_options ${_pg_ldflags})
+  if(_pg_ldflags_ex)
+    list(APPEND _link_options ${_pg_ldflags_ex})
+  endif()
 
-pg_config(_pg_includedir --includedir)
-pg_config(_pg_pkgincludedir --pkgincludedir)
-pg_config(_pg_sharedir --sharedir)
-pg_config(_pg_includedir_server --includedir-server)
-pg_config(_pg_libs --libs)
-pg_config(_pg_ldflags --ldflags)
-pg_config(_pg_ldflags_sl --ldflags_sl)
-pg_config(_pg_ldflags_ex --ldflags_ex)
-pg_config(_pg_pkglibdir --pkglibdir)
-pg_config(_pg_libdir --libdir)
-pg_config(_pg_version --version)
-
-separate_arguments(_pg_ldflags)
-separate_arguments(_pg_ldflags_sl)
-separate_arguments(_pg_ldflags_ex)
-
-set(_server_lib_dirs ${_pg_libdir} ${_pg_pkglibdir})
-set(_server_inc_dirs ${_pg_pkgincludedir} ${_pg_includedir_server})
-string(REPLACE ";" " " _shared_link_options "${_pg_ldflags};${_pg_ldflags_sl}")
-set(_link_options ${_pg_ldflags})
-if(_pg_ldflags_ex)
-  list(APPEND _link_options ${_pg_ldflags_ex})
-endif()
-
-set(PostgreSQL_INCLUDE_DIRS
-    "${_pg_includedir}"
-    CACHE PATH
-          "Top-level directory containing the PostgreSQL include directories.")
-set(PostgreSQL_EXTENSION_DIR
-    "${_pg_sharedir}/extension"
-    CACHE PATH "Directory containing extension SQL and control files")
-set(PostgreSQL_SERVER_INCLUDE_DIRS
-    "${_server_inc_dirs}"
-    CACHE PATH "PostgreSQL include directories for server include files.")
-set(PostgreSQL_LIBRARY_DIRS
-    "${_pg_libdir}"
-    CACHE PATH "library directory for PostgreSQL")
-set(PostgreSQL_LIBRARIES
-    "${_pg_libs}"
-    CACHE PATH "Libraries for PostgreSQL")
-set(PostgreSQL_SHARED_LINK_OPTIONS
-    "${_shared_link_options}"
-    CACHE STRING "PostgreSQL linker options for shared libraries.")
-set(PostgreSQL_LINK_OPTIONS
-    "${_pg_ldflags},${_pg_ldflags_ex}"
-    CACHE STRING "PostgreSQL linker options for executables.")
-set(PostgreSQL_SERVER_LIBRARY_DIRS
-    "${_server_lib_dirs}"
-    CACHE PATH "PostgreSQL server library directories.")
-set(PostgreSQL_VERSION_STRING
-    "${_pg_version}"
-    CACHE STRING "PostgreSQL version string")
-set(PostgreSQL_PACKAGE_LIBRARY_DIR
+  set(PostgreSQL_INCLUDE_DIRS
+      "${_pg_includedir}"
+      CACHE PATH
+            "Top-level directory containing the PostgreSQL include directories."
+  )
+  set(PostgreSQL_EXTENSION_DIR
+      "${_pg_sharedir}/extension"
+      CACHE PATH "Directory containing extension SQL and control files")
+  set(PostgreSQL_SERVER_INCLUDE_DIRS
+      "${_server_inc_dirs}"
+      CACHE PATH "PostgreSQL include directories for server include files.")
+  set(PostgreSQL_LIBRARY_DIRS
+      "${_pg_libdir}"
+      CACHE PATH "library directory for PostgreSQL")
+  set(PostgreSQL_LIBRARIES
+      "${_pg_libs}"
+      CACHE PATH "Libraries for PostgreSQL")
+  set(PostgreSQL_SHARED_LINK_OPTIONS
+      "${_shared_link_options}"
+      CACHE STRING "PostgreSQL linker options for shared libraries.")
+  set(PostgreSQL_LINK_OPTIONS
+      "${_pg_ldflags},${_pg_ldflags_ex}"
+      CACHE STRING "PostgreSQL linker options for executables.")
+  set(PostgreSQL_SERVER_LIBRARY_DIRS
+      "${_server_lib_dirs}"
+      CACHE PATH "PostgreSQL server library directories.")
+  set(PostgreSQL_VERSION_STRING
+      "${_pg_version}"
+      CACHE STRING "PostgreSQL version string")
+  set(PostgreSQL_PACKAGE_LIBRARY_DIR
     "${_pg_pkglibdir}"
     CACHE STRING "PostgreSQL package library directory")
 
-find_program(PG_REGRESS pg_regress HINT
-             ${PostgreSQL_PACKAGE_LIBRARY_DIR}/pgxs/src/test/regress)
-if(NOT PG_REGRESS)
-  message(STATUS "Could not find pg_regress, tests not executed")
-endif()
+  find_program(
+    PG_BINARY postgres
+    PATHS ${PostgreSQL_ROOT_DIRECTORIES}
+    HINTS ${_pg_bindir}
+    PATH_SUFFIXES bin)
 
-set(PostgreSQL_FOUND TRUE)
+  if(NOT PG_BINARY)
+    message(FATAL_ERROR "Could not find postgres binary")
+  endif()
 
-message(STATUS "PostgreSQL version ${PostgreSQL_VERSION_STRING} found")
-message(
-  STATUS
+  message(STATUS "Found postgres binary at ${PG_BINARY}")
+
+  find_program(PG_REGRESS pg_regress HINT
+    ${PostgreSQL_PACKAGE_LIBRARY_DIR}/pgxs/src/test/regress)
+  if(NOT PG_REGRESS)
+    message(STATUS "Could not find pg_regress, tests not executed")
+  endif()
+
+  message(STATUS "PostgreSQL version ${PostgreSQL_VERSION_STRING} found")
+  message(
+    STATUS
     "PostgreSQL package library directory: ${PostgreSQL_PACKAGE_LIBRARY_DIR}")
-message(STATUS "PostgreSQL libraries: ${PostgreSQL_LIBRARIES}")
-message(STATUS "PostgreSQL extension directory: ${PostgreSQL_EXTENSION_DIR}")
-message(STATUS "PostgreSQL linker options: ${PostgreSQL_LINK_OPTIONS}")
-message(
-  STATUS "PostgreSQL shared linker options: ${PostgreSQL_SHARED_LINK_OPTIONS}")
+  message(STATUS "PostgreSQL libraries: ${PostgreSQL_LIBRARIES}")
+  message(STATUS "PostgreSQL extension directory: ${PostgreSQL_EXTENSION_DIR}")
+  message(STATUS "PostgreSQL linker options: ${PostgreSQL_LINK_OPTIONS}")
+  message(
+    STATUS "PostgreSQL shared linker options: ${PostgreSQL_SHARED_LINK_OPTIONS}")
+endif()
 
 # add_postgresql_extension(NAME ...)
 #
@@ -192,9 +207,9 @@ function(add_postgresql_extension NAME)
       STATUS "Building script file ${_script} from template file ${_template}")
   endforeach()
 
-  if (APPLE)
+  if(APPLE)
     set(_link_flags "${_link_flags} -bundle_loader ${PG_BINARY}")
-  endif ()
+  endif()
 
   set_target_properties(
     ${NAME}
